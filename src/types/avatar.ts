9f51e4195fcd49208154b21c08077ba7e9d92f1d
Mario <mariomad2296@gmail.com>
2023-12-07 20:12:38 +0800

    wip

--- src/types/avatar.ts
+++
@@ -1,7 +0,0 @@
-import md5 from 'md5';
-
-export function gravatar(value: string) {
-    const hash = md5(value).trim().toLowerCase();
-    const avatar = `https://www.gravatar.com/avatar/${hash}?s=${150}&d=mp`;
-    return avatar;
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,3 @@
 export type Children = {
     children: React.ReactNode;
 };
-
-export type AuthenticatedUserType = {
-    id?: string;
-    name?: string;
-    email: string;
-    image?: string;
-    randomKey?: string;
-};
